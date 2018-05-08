f5bf31af70dd46dd77ef2173edf2bfb01a32caa2
Manolo Perez <manolope338@gmail.com>
2018-05-08 07:45:49 -0500

    Primer Controller

--- src/controller/FXMLVistaCursosController.java
+++ src/controller/FXMLVistaCursosController.java
@@ -7,9 +7,14 @@
 
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXListView;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.StackPane;
 
 /**
  * @author Manolo Pérez
@@ -18,6 +23,30 @@ import javafx.fxml.Initializable;
  */
 
 public class FXMLVistaCursosController implements Initializable {
+
+  @FXML
+  private StackPane rootPane;
+
+  @FXML
+  private JFXButton btnUser;
+
+  @FXML
+  private JFXButton btnNotifCount;
+
+  @FXML
+  private JFXButton btnInbox;
+
+  @FXML
+  private JFXButton btnHamburger;
+
+  @FXML
+  private JFXListView<?> listCourses;
+
+  @FXML
+  private JFXListView<?> listCategories;
+
+  // ==================================================================================================================
+  // Carga de GUI
 
   /**
    * Inicialización de la escena, carga de datos y animación
@@ -28,6 +57,19 @@ public class FXMLVistaCursosController implements Initializable {
   @Override
   public void initialize(URL url, ResourceBundle rb) {
 
+  }
+
+  // ==================================================================================================================
+  // Eventos FXML
+
+  @FXML
+  void clickInbox(ActionEvent event) {
+    btnNotifCount.setVisible(false);
+  }
+
+  @FXML
+  void clickUser(ActionEvent event) {
+    btnNotifCount.setVisible(true);
   }
 	
 }
